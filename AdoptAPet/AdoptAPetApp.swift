ed7ca7a3e2635163a9f38f7a4213c68efc805cdf
Mohammad Zulqurnain <mohammad.zulqurnain@gmail.com>
2023-02-07 21:14:34 +0000

    Initial Commit

---
+++ AdoptAPet/AdoptAPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdoptAPetApp.swift
+//  AdoptAPet
+//
+//  Created by Mohammad Zulqurnain on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdoptAPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
